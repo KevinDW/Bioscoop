444e71e66a383d140bf359a5df36a407df5206de
Kevin De Win <kevin.de.win@hotmail.com>
2014-08-04 18:59:36 +0200

    Statements verwijderd

--- src/be/kdg/views/Console.java
+++ src/be/kdg/views/Console.java
@@ -11,15 +11,6 @@ public class Console
         try
         {
             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bioscoop", "root", "Iam4life");
-
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM barcode");
-
-            while(rs.next())
-            {
-                System.out.println(rs.getString("code"));
-            }
-
         }
         catch (SQLException e)
         {
