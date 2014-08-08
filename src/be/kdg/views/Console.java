44a0ce1e5fb5d3478d056968166ef87ce3c1dc30
Kevin De Win <kevin.de.win@hotmail.com>
2014-08-08 16:57:31 +0200

    Clean up view

--- src/be/kdg/views/Console.java
+++ src/be/kdg/views/Console.java
@@ -7,17 +7,43 @@ public class Console
 {
     public static void main(String[] args)
     {
+        voegSociaalBerichtToeAanBepaaldeFilm();
+        toonVertoningenVoorBioscoop();
+    }
+
+    private static void voegSociaalBerichtToeAanBepaaldeFilm()
+    {
         try
         {
-            String bioscoop = "Kinepolis Antwerpen";
-            LocalDate startDatum = LocalDate.of(2014, 8, 1);
-            LocalDate eindDatum = LocalDate.now();
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bioscoop", "root", "Iam4life");
+            Statement stmt = con.createStatement();
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
 
-            ResultSet rs = vertoningenVoorBioscoop(bioscoop, startDatum, eindDatum);
+    private static void toonVertoningenVoorBioscoop()
+    {
+        String bioscoop = "Kinepolis Antwerpen";
+        LocalDate startDatum = LocalDate.of(2014, 8, 1);
+        LocalDate eindDatum = LocalDate.now();
 
+        ResultSet rs = vertoningenVoorBioscoop(bioscoop, startDatum, eindDatum);
+
+        System.out.println("Bioscoop\t\t\tZaal\tFilm");
+        System.out.println("==================================");
+
+        try
+        {
             while(rs.next())
             {
-                System.out.println(rs.getString("film.naam"));
+                System.out.printf("%s\t%d\t%s\n",
+                    rs.getString("bioscoop.naam"),
+                    rs.getInt("zaal.zaalNr"),
+                    rs.getString("film.naam")
+                );
             }
         }
         catch (SQLException e)
@@ -26,17 +52,29 @@ public class Console
         }
     }
 
-    private static ResultSet vertoningenVoorBioscoop(String bioscoop, LocalDate startDatum, LocalDate eindDatum) throws SQLException
+    private static ResultSet vertoningenVoorBioscoop(String bioscoop, LocalDate startDatum, LocalDate eindDatum)
     {
-        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bioscoop", "root", "Iam4life");
-        Statement stmt = con.createStatement();
+        try
+        {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bioscoop", "root", "Iam4life");
+            Statement stmt = con.createStatement();
 
-        return stmt.executeQuery("SELECT film.naam FROM programmatie " +
-            "INNER JOIN film ON programmatie.filmId = film.id " +
-            "INNER JOIN zaal ON programmatie.zaalId = zaal.id " +
-            "INNER JOIN bioscoop ON zaal.bioscoopId = bioscoop.id " +
-            "WHERE bioscoop.naam = '" + bioscoop + "' " +
-            "AND programmatie.datum BETWEEN '" + startDatum + "' AND '" + eindDatum + "'"
-        );
+            return stmt.executeQuery
+            (
+                "SELECT bioscoop.naam, zaal.zaalNr, film.naam " +
+                "FROM programmatie " +
+                "INNER JOIN film ON programmatie.filmId = film.id " +
+                "INNER JOIN zaal ON programmatie.zaalId = zaal.id " +
+                "INNER JOIN bioscoop ON zaal.bioscoopId = bioscoop.id " +
+                "WHERE bioscoop.naam = '" + bioscoop + "' " +
+                "AND programmatie.datum BETWEEN '" + startDatum + "' AND '" + eindDatum + "'"
+            );
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
