e816bbd215f132916893b0c91895a8d2d43346ed
Kevin De Win <kevin.de.win@hotmail.com>
2014-08-08 22:14:40 +0200

    Upload newest version

--- src/be/kdg/views/Console.java
+++ src/be/kdg/views/Console.java
@@ -8,7 +8,31 @@ public class Console
     public static void main(String[] args)
     {
         voegSociaalBerichtToeAanBepaaldeFilm();
-        toonVertoningenVoorBioscoop();
+
+        String bioscoop = "Kinepolis Brussel";
+        Date startDatum = Date.valueOf(LocalDate.of(2014, 8, 1));
+        Date eindDatum = Date.valueOf(LocalDate.of(2014, 8, 31));
+
+        ResultSet rs = vertoningenVoorBioscoopTussenTweeDatums(bioscoop, startDatum, eindDatum);
+
+        System.out.println("Bioscoop\t\t\tZaal\tFilm");
+        System.out.println("==================================");
+
+        try
+        {
+            while(rs.next())
+            {
+                System.out.printf("%s\t%d\t\t%s\n",
+                        rs.getString("bioscoop.naam"),
+                        rs.getInt("zaal.zaalNr"),
+                        rs.getString("film.naam")
+                );
+            }
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
     }
 
     private static void voegSociaalBerichtToeAanBepaaldeFilm()
@@ -24,51 +48,27 @@ public class Console
         }
     }
 
-    private static void toonVertoningenVoorBioscoop()
-    {
-        String bioscoop = "Kinepolis Antwerpen";
-        LocalDate startDatum = LocalDate.of(2014, 8, 1);
-        LocalDate eindDatum = LocalDate.now();
-
-        ResultSet rs = vertoningenVoorBioscoop(bioscoop, startDatum, eindDatum);
-
-        System.out.println("Bioscoop\t\t\tZaal\tFilm");
-        System.out.println("==================================");
-
-        try
-        {
-            while(rs.next())
-            {
-                System.out.printf("%s\t%d\t%s\n",
-                    rs.getString("bioscoop.naam"),
-                    rs.getInt("zaal.zaalNr"),
-                    rs.getString("film.naam")
-                );
-            }
-        }
-        catch (SQLException e)
-        {
-            e.printStackTrace();
-        }
-    }
-
-    private static ResultSet vertoningenVoorBioscoop(String bioscoop, LocalDate startDatum, LocalDate eindDatum)
+    private static ResultSet vertoningenVoorBioscoopTussenTweeDatums(String bioscoop, Date startDatum, Date eindDatum)
     {
         try
         {
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bioscoop", "root", "Iam4life");
-            Statement stmt = con.createStatement();
 
-            return stmt.executeQuery
-            (
+            PreparedStatement stmt = con.prepareStatement(
                 "SELECT bioscoop.naam, zaal.zaalNr, film.naam " +
                 "FROM programmatie " +
                 "INNER JOIN film ON programmatie.filmId = film.id " +
                 "INNER JOIN zaal ON programmatie.zaalId = zaal.id " +
                 "INNER JOIN bioscoop ON zaal.bioscoopId = bioscoop.id " +
-                "WHERE bioscoop.naam = '" + bioscoop + "' " +
-                "AND programmatie.datum BETWEEN '" + startDatum + "' AND '" + eindDatum + "'"
+                "WHERE bioscoop.naam = ? " +
+                "AND programmatie.datum BETWEEN ? AND ?"
             );
+
+            stmt.setString(1, bioscoop);
+            stmt.setDate(2, startDatum);
+            stmt.setDate(3, eindDatum);
+
+            return stmt.executeQuery();
         }
         catch (SQLException e)
         {
