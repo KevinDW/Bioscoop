cf05f3d40fedffb7e8b4ecb72b1e9d7a1c5d6e70
Kenneth De Win <knnethdw@gmail.com>
2014-08-09 19:44:00 +0200

    Make ActiveMQ run as as service

--- ActiveMQ.sh
+++ ActiveMQ.sh
@@ -4,11 +4,17 @@ cd /home/vagrant/
 
 wget http://repository.apache.org/content/repositories/snapshots/org/apache/activemq/apache-activemq/5.10-SNAPSHOT/apache-activemq-5.10-20140603.223748-79-bin.tar.gz
 tar zxvf apache-activemq-5.10-20140603.223748-79-bin.tar.gz
+rm -f apache-activemq-5.10-20140603.223748-79-bin.tar.gz
 
-mv apache-activemq-5.10-SNAPSHOT activemq
-cd activemq/bin
+sudo mv /home/vagrant/apache-activemq-5.10-SNAPSHOT /opt/activemq
+sudo ln -sf /opt/activemq/bin/activemq /etc/init.d/
+sudo update-rc.d activemq defaults
 
-chmod 755 activemq
-./activemq setup /home/vagrant/.activemqrc
-./activemq start
-./activemq status
+sudo chmod 755 /opt/activemq/bin/activemq
+
+/opt/activemq/bin/activemq setup /etc/default/activemq
+sudo chown root:nogroup /etc/default/activemq
+sudo chmod 600 /etc/default/activemq
+
+sudo /etc/init.d/activemq start
+sudo /etc/init.d/activemq status
