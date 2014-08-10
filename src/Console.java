4a93893b64fc3931751299b90f1e72e1ddc2ff98
Kenneth De Win <knnethdw@gmail.com>
2014-08-10 13:06:16 +0200

    Clean up Console view

--- src/Console.java
+++ src/Console.java
@@ -1,29 +1,12 @@
 import java.sql.*;
-import java.time.LocalDate;
 
 public class Console
 {
     public static void main(String[] args)
     {
-        String bioscoop = "Kinepolis Antwerpen";
-        Date startDatum = Date.valueOf(LocalDate.of(2014, 8, 1));
-        Date eindDatum = Date.valueOf(LocalDate.of(2014, 8, 31));
-
-        ResultSet rs = vertoningenVoorBioscoopTussenTweeDatums(bioscoop, startDatum, eindDatum);
-
-        System.out.println("Bioscoop\t\t\tZaal\tFilm");
-        System.out.println("==================================");
-
         try
         {
-            while(rs.next())
-            {
-                System.out.printf("%s\t%d\t\t%s\n",
-                        rs.getString("bioscoop.naam"),
-                        rs.getInt("zaal.zaalNr"),
-                        rs.getString("film.naam")
-                );
-            }
+            vertoningenInBioscoopTussenTweeDatums("Kinepolis Antwerpen", new Date(2014, 8, 1), new Date(2014, 8, 31));
         }
         catch (SQLException e)
         {
@@ -31,32 +14,31 @@ public class Console
         }
     }
 
-    private static ResultSet vertoningenVoorBioscoopTussenTweeDatums(String bioscoop, Date startDatum, Date eindDatum)
+    private static void vertoningenInBioscoopTussenTweeDatums(String bioscoop, Date startDatum, Date eindDatum) throws SQLException
     {
-        try
+        Connection con = DriverManager.getConnection("jdbc:mysql://192.168.20.200:3306/bioscoop", "root", "root");
+
+        PreparedStatement stmt = con.prepareStatement(
+            "SELECT bioscoop.naam, zaal.zaalNr, film.naam " +
+            "FROM programmatie " +
+            "INNER JOIN film ON programmatie.filmId = film.id " +
+            "INNER JOIN zaal ON programmatie.zaalId = zaal.id " +
+            "INNER JOIN bioscoop ON zaal.bioscoopId = bioscoop.id " +
+            "WHERE bioscoop.naam = ? AND programmatie.datum BETWEEN ? AND ?"
+        );
+
+        stmt.setString(1, bioscoop);
+        stmt.setDate(2, startDatum);
+        stmt.setDate(3, eindDatum);
+
+        ResultSet rs = stmt.executeQuery();
+
+        System.out.println("Bioscoop\t\t\tZaal\tFilm");
+        System.out.println("==================================");
+
+        while(rs.next())
         {
-            Connection con = DriverManager.getConnection("jdbc:mysql://192.168.20.200:3306/bioscoop", "root", "root");
-
-            PreparedStatement stmt = con.prepareStatement(
-                "SELECT bioscoop.naam, zaal.zaalNr, film.naam " +
-                "FROM programmatie " +
-                "INNER JOIN film ON programmatie.filmId = film.id " +
-                "INNER JOIN zaal ON programmatie.zaalId = zaal.id " +
-                "INNER JOIN bioscoop ON zaal.bioscoopId = bioscoop.id " +
-                "WHERE bioscoop.naam = ? AND programmatie.datum BETWEEN ? AND ?"
-            );
-
-            stmt.setString(1, bioscoop);
-            stmt.setDate(2, startDatum);
-            stmt.setDate(3, eindDatum);
-
-            return stmt.executeQuery();
+            System.out.printf("%s\t%d\t\t%s\n", rs.getString("bioscoop.naam"), rs.getInt("zaal.zaalNr"), rs.getString("film.naam"));
         }
-        catch (SQLException e)
-        {
-            e.printStackTrace();
-        }
-
-        return null;
     }
 }
