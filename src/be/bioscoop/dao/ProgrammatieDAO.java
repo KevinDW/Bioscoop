137f655ec3886c3559927b4eb828cc14e5b60b5e
Kenneth De Win <knnethdw@gmail.com>
2014-08-16 21:26:53 +0200

    Add all method to programmatieDao

--- src/be/bioscoop/dao/ProgrammatieDAO.java
+++ src/be/bioscoop/dao/ProgrammatieDAO.java
@@ -1,6 +1,12 @@
 package be.bioscoop.dao;
 
-import java.sql.Connection;
+import be.bioscoop.models.Film;
+import be.bioscoop.models.Programmatie;
+import be.bioscoop.models.Zaal;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProgrammatieDAO
 {
@@ -9,5 +15,29 @@ public class ProgrammatieDAO
     public ProgrammatieDAO(Connection connection)
     {
         this.connection = connection;
+    }
+
+    public List<Programmatie> all() throws SQLException
+    {
+        PreparedStatement statement = this.connection.prepareStatement(
+            "SELECT * FROM programmatie ORDER BY datum"
+        );
+
+        ResultSet resultSet = statement.executeQuery();
+        List<Programmatie> programmaties = new ArrayList<Programmatie>();
+
+        while (resultSet.next())
+        {
+            Programmatie programmatie = new Programmatie(resultSet.getInt(1));
+
+            programmatie.setDatum(resultSet.getDate(2));
+            programmatie.setBeginUur(resultSet.getTime(3));
+            programmatie.setZaal(new Zaal(resultSet.getInt(4)));
+            programmatie.setFilm(new Film(resultSet.getInt(5)));
+
+            programmaties.add(programmatie);
+        }
+
+        return programmaties;
     }
 }
