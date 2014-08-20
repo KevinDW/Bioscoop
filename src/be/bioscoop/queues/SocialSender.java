6647bd0c56583e65bd7bf000eed686d27edb4630
Kevin De Win <kevin.de.win@hotmail.com>
2014-08-20 13:23:19 +0200

    Update queue sender

--- src/be/bioscoop/queues/SocialSender.java
+++ src/be/bioscoop/queues/SocialSender.java
@@ -14,29 +14,30 @@ public class SocialSender
 
     public void sendMessage() throws Exception
     {
-        //Create a ConnectionFactory
-        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://192.168.20.200:61616");
+        // Create a connection to ActiveMQ
+        Connection connection = new ActiveMQConnectionFactory("tcp://192.168.20.200:61616").createConnection();
 
-        //Create a Connection to ActiveMQ
-        Connection connection = connectionFactory.createConnection();
+        // Start connection
         connection.start();
 
         // Create a Session that allows you to work with activeMQ
-        //False will create a non-transactional session object
-        //True will use transactions
+        // -> false will create a non-transactional session object
+        // -> true will use transactions
         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
         // Create the destination queue (or retrieve it, if it already exists)
-        // Name: TEST.SENDRECEIVE
         Destination destination = session.createQueue("TEST.SENDRECEIVE");
 
         // Create a MessageProducer for the Destination
-        // Non-Persistant: berichten niet door de queue naar disk worden geschreven
         MessageProducer producer = session.createProducer(destination);
+
+        // Non-Persistant: berichten niet door de queue naar disk worden geschreven
         producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
 
         TextMessage message = session.createTextMessage();
         message.setText("Dit is een test!");
+
+        // Send message to queue
         producer.send(message);
     }
 }
