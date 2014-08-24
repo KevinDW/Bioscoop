0779b89a5251474e0c0434fd04c0ca10c5e9f01c
Kenneth De Win <knnethdw@gmail.com>
2014-08-24 16:56:17 +0200

    Update Social receiver + schema

--- src/be/bioscoop/queues/SocialReceiver.java
+++ src/be/bioscoop/queues/SocialReceiver.java
@@ -12,6 +12,8 @@ import org.jdom.JDOMException;
 import org.jdom.input.SAXBuilder;
 
 import javax.jms.*;
+import javax.xml.XMLConstants;
+import javax.xml.validation.SchemaFactory;
 import java.io.IOException;
 import java.io.StringReader;
 import java.sql.Date;
@@ -38,16 +40,34 @@ public class SocialReceiver
         // Maak een consumer aan die berichten van de queue ontvangt
         MessageConsumer consumer = session.createConsumer(destination);
 
+        // Haal bericht binnen uit queue
         Message message = consumer.receive(1000);
-        TextMessage textMessage = (TextMessage) message;
 
+        // Bericht toevoegen aan database
+        addMessageToDatabase((TextMessage) message);
+
+        // Consumer sluiten
+        consumer.close();
+
+        // Sessie sluiten
+        session.close();
+
+        // Connectie sluiten
+        amqConnection.close();
+    }
+
+    private void addMessageToDatabase(TextMessage textMessage) throws SQLException, JDOMException, IOException, JMSException
+    {
         java.sql.Connection dbConnection = Database.connect();
 
         FilmDAO filmDAO = new FilmDAO(dbConnection);
         SocialDAO socialDAO = new SocialDAO(dbConnection);
 
+        String text = textMessage.getText();
+        StringReader xml = new StringReader(text);
+
         SAXBuilder sax = new SAXBuilder();
-        Document doc = sax.build(new StringReader(textMessage.getText()));
+        Document doc = sax.build(xml);
         Element root = doc.getRootElement();
 
         for (Object object : root.getChildren())
@@ -64,13 +84,6 @@ public class SocialReceiver
             );
         }
 
-        // Consumer sluiten
-        consumer.close();
-
-        // Sessie sluiten
-        session.close();
-
-        // Connectie sluiten
-        amqConnection.close();
+        Database.close();
     }
 }
